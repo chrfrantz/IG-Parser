e92f0479a4c7c7362e2d5168a08294b896354a2f
Christopher Frantz <christopher.frantz@ntnu.no>
2021-04-08 15:35:35 +0200

    Refined example.

--- web/converter/Examples.go
+++ web/converter/Examples.go
@@ -3,7 +3,7 @@ package converter
 import "IG-Parser/tree"
 
 // Default example statement
-var ANNOTATED_STATEMENT = "(National Organic Program's Program Manager), Cex(on behalf of the Secretary), D(may) I(inspect and), I(sustain (review [AND] (refresh [AND] drink))) Bdir(approved (certified production and [AND] handling operations and [AND] accredited certifying agents)) Cex(for compliance with the (Act or [XOR] regulations in this part))"
+var ANNOTATED_STATEMENT = "A,p(Any) A(National Organic Program's Program Manager), Cex(on behalf of the Secretary), D(may) I(inspect and), I(sustain (review [AND] (refresh [AND] drink))) Bdir(approved (certified production and [AND] handling operations and [AND] accredited certifying agents)) Cex(for compliance with the (Act or [XOR] regulations in this part))"
 // Default example ID
 var STATEMENT_ID = "650"
 // Help for raw statement field
