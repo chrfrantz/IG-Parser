41d27be2d7bbf43cb8cfa521a32f617d4824e30c
Christopher Frantz <christopher.frantz@ntnu.no>
2021-04-30 15:28:49 +0200

    Added error checking for deployment.

--- deploy.sh
+++ deploy.sh
@@ -11,18 +11,39 @@ echo "Initiating (re)deployment of latest version of IG-Parser ..."
 # Create logs folder if it does not already exist
 if [ ! -d ./logs ]; then
   mkdir -p ./logs;
+  if [ $? -ne 0 ]; then
+    echo "Error during folder creation." 
+    exit 1
+  fi
 fi
+
 
 # Tear down current version 
 docker-compose down
+if [ $? -ne 0 ]; then
+  echo "Stopping of service." 
+  exit 1
+fi
+
 
 # Shortcut to clean up generated docker image
-docker rmi ig-parser_web
+#docker rmi ig-parser_web
+
 
 # Pull latest version
 git pull
+if [ $? -ne 0 ]; then
+  echo "Error when pulling latest version of repo." 
+  exit 1
+fi
+
 
 # Deploy
-docker-compose up -d
+docker-compose up -d --build
+if [ $? -ne 0 ]; then
+  echo "Error during deployment." 
+  exit 1
+fi
+
 
 echo "Latest version should be deployed (check output for errors)"
