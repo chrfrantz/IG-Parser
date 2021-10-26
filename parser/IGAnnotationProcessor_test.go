5681a864836f14a1a27b043cade8378013e0eb0b
chrfrantz <cf@christopherfrantz.org>
2021-10-27 01:56:57 +0200

    Fixed test.

--- parser/IGAnnotationProcessor_test.go
+++ parser/IGAnnotationProcessor_test.go
@@ -37,7 +37,7 @@ func TestExtractComponentLinkageNonAggregateLinkages(t *testing.T) {
 
 func TestExtractComponentLinkageAggregateLinkages(t *testing.T) {
 
-	text := "A,p(Certified) A,p1(non-suspended) A,p1(previously reviewed) A1(Operator) or A,p2(recognized) A2(Handler) D(must not) I((produce [AND] trade))"
+	text := "A,p(Certified) A1,p1(non-suspended) A1,p1(previously reviewed) A1(Operator) or A2,p(recognized) A2(Handler) D(must not) I((produce [AND] trade))"
 
 	// Indicates whether implicitly linked components (e.g., I(one) I(two)) are aggregated into a single component
 	tree.AGGREGATE_IMPLICIT_LINKAGES = true
