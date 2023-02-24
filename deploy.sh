499d4dfa1802c7ac69911261cfc89e3177c925c3
Christopher Frantz <cf@christopherfrantz.org>
2023-02-24 18:13:11 +0100

    Harden deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This file is part of the IG Parser project.
+# This file is part of the IG Parser project (URL: https://github.com/chrfrantz/IG-Parser).
 # Maintainer: Christopher Frantz (cf@christopherfrantz.org)
 
 # Clears current instance of IG Parser and redeploys after pulling from git repository.
@@ -9,9 +9,9 @@
 echo "Initiating (re)deployment of latest version of the IG Parser ..."
 
 # Create logs folder if it does not already exist
-if [ ! -d ./logs ]; then
+if [[ ! -d ./logs ]]; then
   echo "Creating './logs' folder ..."
-  if [ "$(mkdir -p ./logs)" -ne 0 ]; then
+  if [[ "$(mkdir -p ./logs)" -ne 0 ]]; then
     echo "Error during folder creation." 
     exit 1
   fi
@@ -19,23 +19,23 @@ fi
 
 # Tear down current version 
 echo "Undeploying running IG Parser instance ..."
-if [ "$(docker-compose down)" -ne 0 ]; then
+if [[ "$(docker-compose down)" -ne 0 ]]; then
   echo "Stopping of service." 
   exit 1
 fi
 
 # Pull latest version
 echo "Retrieving latest version ..."
-if [ "$(git pull)" -ne 0 ]; then
+if [[ "$(git pull)" -ne 0 ]]; then
   echo "Error when pulling latest version of repo." 
   exit 1
 fi
 
 # Create docker network if not already existing
 echo "Checking network setup ..."
-if [ "$(docker network inspect tunnel_network > /dev/null)" -ne 0 ]; then
+if [[ "$(docker network inspect tunnel_network > /dev/null)" -ne 0 ]]; then
   echo "Dedicated docker network does not exist. Creating 'tunnel_network' ..."
-  if [ "$(docker network create tunnel_network)" -ne 0 ];  then
+  if [[ "$(docker network create tunnel_network)" -ne 0 ]];  then
     echo "Network creation failed."
     exit 1
   fi
@@ -43,14 +43,14 @@ fi
 
 # Deploy
 echo "Deploying latest version of IG Parser ..."
-if [ "$(docker-compose up -d --build)" -ne 0 ]; then
+if [[ "$(docker-compose up -d --build)" -ne 0 ]]; then
   echo "Error during deployment." 
   exit 1
 fi
 
 # Remove remaining build containers
 echo "Removing all intermediate images created during build process ..."
-if [ "$(docker image prune --filter label=stage=builder -f)" -ne 0 ]; then
+if [[ "$(docker image prune --filter label=stage=builder -f)" -ne 0 ]]; then
   echo "Error when deleting intermediate images created during build."
   exit 1
 fi
